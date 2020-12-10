7f71e4d16e40621ff7b5f48eb57bfa1d5842067a
Alexandre Gros <xelaflash@gmail.com>
2020-12-10 13:57:46 -0400

    remove home specific layout due too css bug

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -4,24 +4,13 @@ import Fonts from './src/styles/Fonts';
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './src/components/Layout';
-import HomeLayout from './src/components/HomeLayout';
 
 export function wrapPageElement({ element }) {
-  const url = typeof window !== 'undefined' ? window.location.pathname : '';
-  if (url !== '/') {
-    return (
-      <Layout>
-        <GlobalStyles />
-        <Fonts />
-        {element}
-      </Layout>
-    );
-  }
   return (
-    <HomeLayout>
+    <Layout>
       <GlobalStyles />
       <Fonts />
       {element}
-    </HomeLayout>
+    </Layout>
   );
 }
