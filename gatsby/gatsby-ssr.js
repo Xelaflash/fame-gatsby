4d95bce6cc6b8f35cef5040589ac7033e07191b5
Alexandre Gros <xelaflash@gmail.com>
2020-12-10 13:21:18 -0400

    wrapPageElement

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './src/components/Layout';
 import HomeLayout from './src/components/HomeLayout';
 
-export function wrapRootElement({ element }) {
+export function wrapPageElement({ element }) {
   const url = typeof window !== 'undefined' ? window.location.pathname : '';
   if (url !== '/') {
     return (
