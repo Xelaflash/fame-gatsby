215d8feed927fb6aa6fbf30a3e1ed4b67c43eb57
Alexandre Gros <xelaflash@gmail.com>
2020-12-17 11:41:21 -0400

    📝 remove comments

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,10 +1,7 @@
 import path from 'path';
-// import fetch from 'isomorphic-fetch';
 
 async function turnRecipesIntoPages({ graphql, actions }) {
-  // 1. Get a template for this page
   const recipeTemplate = path.resolve('./src/templates/RecipeTemplate.js');
-  // 2. query all recipes (via node API)
   const { data } = await graphql(`
     query {
       recipes: allSanityRecipe {
@@ -17,14 +14,11 @@ async function turnRecipesIntoPages({ graphql, actions }) {
       }
     }
   `);
-  console.log(data);
-  // 3. loop over each pizza and create a page for that pizza
   data.recipes.nodes.forEach((recipe) => {
     actions.createPage({
-      // specify the URL for the page
       path: `/recipe/${recipe.slug.current}`,
       component: recipeTemplate,
-      // in order to pass data from this createPage method to the actual template you need context
+
       context: {
         slug: recipe.slug.current,
       },
@@ -33,7 +27,5 @@ async function turnRecipesIntoPages({ graphql, actions }) {
 }
 
 export async function createPages(params) {
-  // Create pages dynamically
-  // wait for all promises to be resolved before finishing them all
   await Promise.all([turnRecipesIntoPages(params)]);
 }
