55dfb6b3003bfa5ecf147aa1621837a5a0ea6418
Alexandre Gros <xelaflash@gmail.com>
2021-01-07 12:27:16 -0400

    🚀 modal all godd ==> 🚧 styling to do

--- gatsby/src/components/food/ArtImgModal.js
+++ gatsby/src/components/food/ArtImgModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Img from 'gatsby-image';
+import Img from 'gatsby-image';
 import '../../styles/ArtImgModalStyles.scss';
 
 const Modal = (props) => {
@@ -14,34 +14,50 @@ const Modal = (props) => {
     display: displayModal ? 'block' : 'none',
   };
 
-  return (
-    <div
-      className="modal"
-      onClick={closeModal}
-      style={divStyle}
-      onKeyPress={closeModal}
-      role="button"
-      tabIndex="0"
-    >
+  if (activeItem !== '') {
+    return (
       <div
-        className="modal-content"
-        onClick={(e) => e.stopPropagation()}
-        aria-hidden="true"
+        className="modal"
+        onClick={closeModal}
+        style={divStyle}
+        onKeyPress={closeModal}
+        role="button"
+        tabIndex="0"
       >
-        <span
-          className="close"
-          onClick={closeModal}
-          onKeyPress={closeModal}
-          role="button"
-          tabIndex="0"
+        <div
+          className="modal-content"
+          onClick={(e) => e.stopPropagation()}
+          aria-hidden="true"
         >
-          &times;
-        </span>
-        {activeItem.name}
-        {activeItem.artDescription}
+          <span
+            className="close"
+            onClick={closeModal}
+            onKeyPress={closeModal}
+            role="button"
+            tabIndex="0"
+          >
+            &times;
+          </span>
+
+          <h3>{activeItem.name}</h3>
+
+          <p className=""> by {activeItem.artist.name}</p>
+          <Img
+            fluid={activeItem.artist.image.asset.fluid}
+            alt={`${activeItem.artist.name} - F.A.M.E Artist`}
+            className="avatar"
+          />
+          <Img
+            fluid={activeItem.image.asset.fluid}
+            alt={`${activeItem.name} - F.A.M.E Art`}
+            className="art_img"
+          />
+          <p>{activeItem.artDescription}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <></>;
 };
 
 export default Modal;
