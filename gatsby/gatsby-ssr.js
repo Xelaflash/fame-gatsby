e36170ff116403753acd137b03ee9e435962609c
Alexandre Gros <xelaflash@gmail.com>
2021-07-14 10:08:36 -0400

    fix meta issue with react helmet

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -4,6 +4,21 @@ import Fonts from './src/styles/Fonts';
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
+  const headComponents = getHeadComponents();
+
+  headComponents.sort((a, b) => {
+    if (a.props && a.props['data-react-helmet']) {
+      return 0;
+    }
+    return 1;
+  });
+  replaceHeadComponents(headComponents);
+};
+
 export function wrapPageElement({ element }) {
   return (
     <>
